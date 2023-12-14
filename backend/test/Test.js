586c684f1d119cf1d1a039f3190591fcfc29c72a
harsh788 <Harsh.Shah@iiitb.ac.in>
2023-12-14 18:23:42 +0530

    Modified test cases

--- backend/test/Test.js
+++ backend/test/Test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const logger = require('../logger');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -15,8 +14,6 @@ describe('Vote Controller', () => {
             .end((err, res) => {
                 if(err){
                     console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
                     return done(err);
                 }
 
@@ -27,43 +24,9 @@ describe('Vote Controller', () => {
                     expect(res.body).to.have.property('candidate_list');
                     expect(res.body).to.have.property('student_list');
                     done();
-
-                    logger.info('Test passed');
                 }
                 else{
                     done(new Error('Unexpected response body format'));
-                    logger.error('Unexpected response body format');
-                    logger.error('Test failed');
-                }
-            });
-        });
-    });
-
-    // test vote_update_get function
-    describe('/GET vote_update_get/:id', () => {
-        it('it should GET updated vote details', (done) => {
-            const voteID = '657744ad4a78ab65f4ea2c44';
-            chai.request('http://localhost:5000')
-            .get('/dashboard/vote/' + voteID + '/update')
-            .end((err, res) => {
-                if(err){
-                    console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
-                    return done(err);
-                }
-
-                if(typeof res.body === 'object' && res.body !== null){
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('candidate_list').to.be.an('array');
-                    done();
-
-                    logger.info('Test passed');
-                }
-                else{
-                    done(new Error('Unexpected response body format'));
-                    logger.error('Unexpected response body format');
-                    logger.error('Test failed');
                 }
             });
         });
@@ -80,42 +43,18 @@ describe('Student Controller', () => {
             .end((err, res) => {
                 if(err){
                     console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
                     return done(err);
                 }
 
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.an('array');
                 done();
-
-                logger.info('Test passed');
             });
         });
     });
 
     // test student_detail function
     describe('/GET student_detail/:id', () => {
-        it('it should GET the detail page for a student with valid ID', (done) => {
-            const student_id = '657744ad4a78ab65f4ea2c35';
-            chai.request('http://localhost:5000')
-            .get('/dashboard/student/' + student_id)
-            .end((err, res) => {
-                if(err){
-                    console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
-                    return done(err);
-                }
-
-                expect(res).to.have.status(200);
-                expect(res).to.be.html;
-                done();
-
-                logger.info('Test passed');
-            });
-        });
-
         it('it should return a 500 status for an invalid ID', (done) => {
             const student_id = '657744ad4a78ab65f4ea2c';
             chai.request('http://localhost:5000')
@@ -123,15 +62,11 @@ describe('Student Controller', () => {
             .end((err, res) => {
                 if(err){
                     console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
                     return done(err);
                 }
 
                 expect(res).to.have.status(500);
                 done();
-
-                logger.info('Test passed');
             });
         });
     });
@@ -147,16 +82,12 @@ describe('Candidate Controller', () => {
             .end((err, res) => {
                 if(err){
                     console.error(err);
-                    logger.error('Error during test: ' + toString(err));
-                    logger.error('Test failed');
                     return done(err);
                 }
 
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.an('array');
                 done();
-
-                logger.info('Test passed');
             });
         });
     });
