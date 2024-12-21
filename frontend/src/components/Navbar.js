bf89ca80136d1ada038360b28625a343147f56ca
harsh788 <Harsh.Shah@iiitb.ac.in>
2024-12-22 00:51:54 +0530

    Added interactive navbar functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 import {
   Navbar,
   NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  Button
+  Button,
+  NavbarText,
 } from 'reactstrap';
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    if(token) {
+      setUser(jwtDecode(token));
+    }
+  });
 
   const toggle = () => setIsOpen(!isOpen);
 
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    setUser(null);
+    window.location.href='/';
+  }
+
   return (
     <div>
-      <Navbar color="danger" dark>
+      <Navbar color="success" dark>
         <NavbarBrand href="/">SAC Election Portal</NavbarBrand>
         <Nav>
           <NavItem>
@@ -33,9 +49,25 @@ const Navigation = () => {
               Votes
             </NavLink>
           </NavItem>
-          <NavItem>
-            <Button color="secondary" href="/login">Login</Button>
-          </NavItem>
+          {user ? 
+            <>
+              <NavItem>
+                <NavbarText style={{margin: 4, color: 'white', fontWeight: 'bold'}}>Hi there, {user.name}!</NavbarText>
+              </NavItem>
+              <NavItem>
+                <Button color="danger" onClick={handleLogout} style={{margin: 4}}>Logout</Button>
+              </NavItem>
+            </>
+          :
+            <>
+              <NavItem>
+                <Button color="primary" href="/signup" style={{margin: 4}}>SignUp</Button>
+              </NavItem>
+              <NavItem>
+                <Button color="primary" href="/login" style={{margin: 4}}>Login</Button>
+              </NavItem>
+            </>
+          }
         </Nav>
       </Navbar>
     </div>
