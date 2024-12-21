f31d6c586abfa591458fc6b3c25b97f2015ce59d
harsh788 <Harsh.Shah@iiitb.ac.in>
2024-12-22 00:41:32 +0530

    Added password and hashing for Student

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
     name: {type: String, required: true, maxLength: 100},
     roll_number: {type: String, required: true, minLength: 9, maxLength: 10},
+    password: {type: String, required: true, minLength: 5},
     batch: {
         type: String, 
         required: true, 
@@ -12,6 +14,23 @@ const studentSchema = new Schema({
         default: "IMT2020",
     },
 });
+
+// Hashing the password before saving
+studentSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch(error) {
+        next(error);
+    }
+});
+
+// Compare password before login
+studentSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 // Virtual for student url
 studentSchema.virtual("url").get(function() {
